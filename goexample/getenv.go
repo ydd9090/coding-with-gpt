26a2d280d654692ce877705ef8927bd519eb93d3
liqiong.278 <liqiong.278@bytedance.com>
2023-03-27 20:09:20 +0800

    add read file code

---
+++ goexample/getenv.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"bufio"
+	)
+
+type TestFile struct{
+	FileName string
+	SearchPath string
+	AbsPath string
+}
+
+//检查文件是否存在
+func (t *TestFile) IsExist() bool{
+	err := filepath.Walk(t.SearchPath, func(path string, info os.FileInfo, err error) error {
+		//如果遍历目录出现错误，则返回错误信息
+		if err != nil {
+			return err
+		}
+		//如果遍历到的是目录，则继续遍历
+		if info.IsDir(){
+			return nil
+
+		}
+		//遍历到目标文件，则返回SkipDir，停止遍历
+		if info.Name() == t.FileName{
+			t.AbsPath = path
+		return filepath.SkipDir
+	}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
+
+func (t *TestFile) GetAbsPath() string{
+	return t.AbsPath
+}
+
+//按行读取文件，返回一个字符串切片
+func (t *TestFile) ReadLines() []string{
+
+	file, err := os.Open(t.AbsPath)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer file.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return lines
+
+}
+
+
+func (t *TestFile) GetTotalRows() int{
+	file , err := os.Open(t.AbsPath)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	totalRows := 0
+	for scanner.Scan(){
+		totalRows++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	return totalRows
+}
